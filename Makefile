c4b77f4e95f4c99aee3dfa3e0de1b07031ef2397
Hiroshi Miyake <7939012701@mail.ecc.u-tokyo.ac.jp>
2014-11-09 02:32:25 +0900

    consider null distribution

--- Makefile
+++ Makefile
@@ -1,11 +1,58 @@
+CXX = g++
+CXXFLAGS += -Wall -Wextra -MMD -std=c++11 -O3
+LDFLAGS += -lm
 TARGET = gsmf
-SRCDIR = src
-all p prof d debug:
-	@cd $(SRCDIR) && make $(MAKECMDGOALS)
-	@-[ -e $(SRCDIR)/$(TARGET) ] && cp $(SRCDIR)/$(TARGET) . ||:
+SRCIGNORE =
+SRC_DIR = src
+SRCS = $(filter-out $(SRC_DIR)/$(SRCIGNORE), $(wildcard $(SRC_DIR)/*.cc))
+OBJS = $(SRCS:.cc=.o)
+DEPS = $(OBJS:.o=.d)
+INI_DIR = ~/template
+INIT = for.cc
+RM = rm -fv
+MODE = release
+ifeq ($(MODE),debug)
+	CXXFLAGS += -ggdb -DNDEBUG
+	CXXFLAGS := $(filter-out -O3, $(CXXFLAGS))
+endif
+ifeq ($(MODE),profile)
+	CXXFLAGS += -pg -fno-inline-functions-called-once -fno-optimize-sibling-calls
+endif
 
-c clean:
-	@cd $(SRCDIR) && make $(MAKECMDGOALS)
-	@-[ -e $(SRCDIR)/$(TARGET) ] && cp $(SRCDIR)/$(TARGET) . ||:
-	@-[ -e $(TARGET) ] && rm -v $(TARGET) ||:
+.PHONY:all information
+all: information $(TARGET)
+information:
+ifneq ($(MODE),release)
+ifneq ($(MODE),debug)
+ifneq ($(MODE),profile)
+	@echo "Invalid build mode."
+	@exit 1
+endif
+endif
+endif
+	@echo "Building $(TARGET) on "$(MODE)" MODE"
+	@echo "Flags:" $(CXXFLAGS)
+	@echo "............................."
 
+$(TARGET): $(OBJS) Makefile
+	@echo linking $(OBJS)
+	@$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)
+
+-include $(DEPS)
+%.o:%.cc Makefile
+	@echo compiling $<
+	@$(CXX) -c $(CXXFLAGS) -o $@ $<
+
+.SILENT:debug profile clean init d prof p c i
+debug d:
+	$(MAKE) MODE=debug   --always-make --no-print-directory
+profile prof p:
+	$(MAKE) MODE=profile --always-make --no-print-directory
+clean c:
+	$(RM) $(TARGET) $(OBJS) $(DEPS) gmon.out .*.swp .*.swo *~
+init i:
+	-@if [ -e $(SRC_DIR)/main.cc ] ;then \
+	echo main.cc already exists. && echo remove main.cc and try again. ;\
+	else \
+	if [ ! -e $(SRC_DIR) ] ; then mkdir $(SRC_DIR) ; fi && \
+	cp -v $(INI_DIR)/$(INIT) $(SRC_DIR)/main.cc; fi
--- src/main.cc
+++ src/main.cc
@@ -4,47 +4,60 @@
 #include <vector>
 #include <ctime>
 #include <random>
-using namespace std;
 
 struct Gsmf /* Gibbs Sampling Motif Finder */
 {
-    vector<vector<int>> nseqs; /* sequence set */
-    vector<int> startPos; /* start position set */
-    vector<vector<double>> prof; /* profile matrix */
-    string const alphs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; /* allowed alphabets */
+    std::vector<std::vector<int>> nseqs; /* sequence set */
+    std::vector<int> startPos; /* start position set */
+    std::vector<std::vector<double>> prof; /* profile matrix */
+    std::vector<double> null0; /* total character frequency */
+    std::string const alphs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; /* allowed alphabets */
     int mlen; /* motif length */
-    int sampler(vector<double> const &d)
+    int sampler(std::vector<double> const &d) /* sample from given histogram */
     {
-        random_device rd;
-        mt19937 generator(rd());
-        discrete_distribution<int> distribution(d.begin(), d.end());
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::discrete_distribution<int> distribution(d.begin(), d.end());
         return distribution(generator);
     }
-    Gsmf(vector<string> const &s, int const m): mlen(m)
+    Gsmf(std::vector<std::string> const &s, int const m): mlen(m)
     {
         nseqs.resize(s.size());
-        startPos.resize(s.size());
+        startPos.resize(s.size(), 0.);
+        null0.resize(alphs.size(), 1. /* pseudocount */);
         for (size_t i = 0; i < s.size(); ++ i) {
             nseqs[i].resize(s[i].size());
-            transform(s[i].begin(), s[i].end(), nseqs[i].begin(), [&](char c){return alphs.find(c);});
+            std::transform(s[i].begin(), s[i].end(), nseqs[i].begin(), [&](char c){return alphs.find(c);});
+            std::for_each(nseqs[i].begin(), nseqs[i].end(), [&](int c){++ null0[c];});
         }
-        prof = vector<vector<double>>(m, vector<double>(alphs.size(), 1. / alphs.size()));
+        prof = std::vector<std::vector<double>>(m + 1, std::vector<double>(alphs.size(), 1. / alphs.size()));
+    }
+    void normalize(std::vector<double> v)
+    {
+        double sum = 0.;
+        for (size_t i = 0; i < v.size(); ++ i) sum += v[i];
+        for (size_t i = 0; i < v.size(); ++ i) v[i] /= sum;
     }
     void train(unsigned int const maxIt)
     {
         srand(static_cast<unsigned int>(time(NULL)));
-        transform(nseqs.begin(), nseqs.end(), startPos.begin(), [&](vector<int> &s){return rand() % (s.size() - mlen + 1);});
-        for (size_t i = 0, count = 0; count < nseqs.size() && i < maxIt; ++ i) {
+        for (size_t i = 0; i < nseqs.size(); ++ i) startPos[i] = rand() % (nseqs[i].size() - mlen + 1);
+        for (size_t i = 0, count = 0; count < 10 + nseqs.size() && i < maxIt; ++ i) {
             int hold = rand() % nseqs.size(); /* choose one sequence without which profile table is constructed */
-            fill_n(prof.begin(), mlen, vector<double>(alphs.size(), 1. / nseqs.size() /* pseudocount */));
-            for (size_t j = 0; j < nseqs.size(); ++ j)
+            std::fill_n(prof.begin(), mlen + 1, std::vector<double>(alphs.size(), 1. /* pseudocount */));
+            prof[0] = null0;
+            for (size_t j = 0; j < nseqs.size(); ++ j) /* update profile and null */
                 if (hold != (int)j)
-                    for (int k = 0; k < mlen; ++ k)
-                        prof[k][nseqs[j][startPos[j] + k]] += 1. / nseqs.size(); /* each row sums upto 1 (with pseudocount) */
-            vector<double> weightNew(nseqs[hold].size() - mlen + 1, 1.);
+                    for (int k = 0; k < mlen; ++ k) {
+                        ++ prof[k + 1][nseqs[j][startPos[j] + k]];
+                        -- prof[0][nseqs[j][startPos[j] + k]];
+                    }
+            for (size_t j = 0; j < nseqs[hold].size(); ++ j) -- prof[0][nseqs[hold][j]]; /* update null */
+            for (size_t j = 0; j < prof.size(); ++ j) normalize(prof[j]);
+            std::vector<double> weightNew(nseqs[hold].size() - mlen + 1, 1.);
             for (size_t j = 0; j < nseqs[hold].size() - mlen + 1; ++ j)
                 for (int k = 0; k < mlen; ++ k)
-                    weightNew[j] *= prof[k][nseqs[hold][j + k]];
+                    weightNew[j] *= prof[k + 1][nseqs[hold][j + k]] / prof[0][nseqs[hold][j + k]]; /* weight of startPos */
             double old = startPos[hold];
             count = old == (startPos[hold] = sampler(weightNew))? count + 1: 0; /* simple criteria of convergence */
         }
@@ -53,9 +66,9 @@ struct Gsmf /* Gibbs Sampling Motif Finder */
 
 int main(int const, char const * const argv[])
 {
-    ifstream fasta(argv[1]);
-    vector<string> seqs;
-    for (string s; getline(fasta, s);)
+    std::ifstream fasta(argv[1]);
+    std::vector<std::string> seqs;
+    for (std::string s; getline(fasta, s);)
         if ('>' == s.at(0))
             seqs.push_back("");
         else
@@ -64,6 +77,6 @@ int main(int const, char const * const argv[])
     Gsmf g(seqs, mlen);
     g.train(1000000);
     for (size_t i = 0; i < seqs.size(); ++ i)
-        cerr << g.startPos[i] << "\t"<< seqs[i].substr(g.startPos[i], mlen) << endl;
+        std::cerr << g.startPos[i] << "\t"<< seqs[i].substr(g.startPos[i], mlen) << std::endl;
     return 0;
 }
